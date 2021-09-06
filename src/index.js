7638529af0ecbc3c6f2826b00508f3cbabc570bb
Maki0419-git <yoshinomaki90530@gmail.com>
2021-09-06 17:43:12 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from 'react'
+import ReactDom from 'react-dom'
+import webSocket from 'socket.io-client'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Main = () => {
+  const [ws, setWs] = useState(null)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const connectWebSocket = () => {
+    //開啟
+    setWs(webSocket('http://localhost:3000'))
+    //連線至剛剛執行 server.js 的 http://localhost:3000
+  }
+
+  useEffect(() => {
+    if (ws) {
+      //連線成功在 console 中打印訊息
+      console.log('success connect!')
+      //設定監聽
+      initWebSocket()
+    }
+  }, [ws])
+
+  const initWebSocket = () => {
+    //對 getMessage 設定監聽，如果 server 有透過 getMessage 傳送訊息，將會在此被捕捉
+    ws.on('getMessage', message => {
+      console.log(message)
+    })
+
+    ws.on('getMessageAll', message => {
+      console.log(message)
+    })
+    ws.on('getMessageLess', message => {
+      console.log(message)
+    })
+  }
+
+  const sendMessage = (name) => {
+    ws.emit(name, '收到訊息囉！')
+  }
+
+
+  return (
+    <div>
+      <input type='button' value='連線' onClick={connectWebSocket} />
+      <input type='button' value='送出訊息，只有自己收到回傳' onClick={() => { sendMessage('getMessage') }} />
+      <input type='button' value='送出訊息，讓所有人收到回傳' onClick={() => { sendMessage('getMessageAll') }} />
+      <input type='button' value='送出訊息，除了自己外所有人收到回傳' onClick={() => { sendMessage('getMessageLess') }} />
+    </div>
+  )
+}
+
+ReactDom.render(<Main />, document.getElementById('root'))
